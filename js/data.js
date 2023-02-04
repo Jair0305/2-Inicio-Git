0a365ec05fa9c10ccc09e54122453c2cb47dd812
Jair Chavez Islas <chamaco_03@hotmail.es>
2023-02-04 00:59:01 -0600

    add js

---
+++ js/data.js
@@ -0,0 +1,33 @@
+const songs = 
+[
+    {
+        name: 'song 1',
+        path '../music/Song 1.mp3',
+        artist: 'yooo',
+        cover: '../images/cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path '../music/Song 2.mp3',
+        artist: 'yooo',
+        cover: '../images/cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path '../music/Song 3.mp3',
+        artist: 'yooo',
+        cover: '../images/cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path '../music/Song 4.mp3',
+        artist: 'yooo',
+        cover: '../images/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path '../music/Song 5.mp3',
+        artist: 'yooo',
+        cover: '../images/cover 5.png'
+    }
+]
